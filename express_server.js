c4947ad73fc29612402eca470903fa9d0aa4970b
Robbie Prokop <robbieprokop@gmail.com>
2022-04-18 14:19:37 +0000

    created URLS ejs and added bootstrap cdn

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { Template } = require("ejs");
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -28,6 +29,12 @@ app.get("/set", (req, res) => {
 
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
+});
+
+//adding pages with urls
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
