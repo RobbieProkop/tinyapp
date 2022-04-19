204e6f97281ef2d41354b6199ff8e55c5f240670
Robbie Prokop <robbieprokop@gmail.com>
2022-04-19 19:58:30 +0000

    completed login and logout

--- express_server.js
+++ express_server.js
@@ -2,34 +2,50 @@ const { Template } = require("ejs");
 const express = require("express");
 const { redirect } = require("express/lib/response");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const { response } = require("express");
 
 //cookies! Yum!
 const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 //middleware
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // middleware
+//this logs are status codes and requests. Pretty cool
 const morgan = require("morgan");
+const { cookie } = require("request");
 app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 
 // variable containing urls
 let urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "9sm5xK": "https://nepallife.org",
+  b2xVn2: "https://dhamma.org",
 };
 
+// used to generate a random string for the short URL
 const generateRandomString = () => {
   return Math.random().toString(36).slice(2, 8);
 };
 
+//for login (if no cookie is present)
 app.post("/login", (req, res) => {
-  const cookie = req.cookie.username;
+  const cookieID = req.body.username;
+  res.cookie("username", cookieID);
+  // redirect to a specified page
+  res.redirect("/urls");
+});
+
+// for logout (if a cookie is present)
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  //must render a .ejs file to be readable html
+  // .render can send variables to the front end from the server if it is not present in the HTML
+  // res.render("/urls_index.ejs");
   res.redirect("/urls");
 });
 
@@ -59,8 +75,12 @@ app.post("/urls/:id", (req, res) => {
 
 // asks to GET the urls page from the server
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
+  console.log(req.cookies["username"]);
 });
 
 // used to get the page to input a new url
@@ -71,9 +91,11 @@ app.get("/urls/new", (req, res) => {
 // get the page based on the shorturl
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
+  //creating an object
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -86,12 +108,12 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 // catch all for errors
-// app.get("*", (req, res) => {
-//   // response.status(404);
-//   // response.send("my custom 404 page");
-//   // same thing, just shorthand
-//   return response.status(404).send("my custom 404 page");
-// });
+app.get("/404", (req, res) => {
+  // response.status(404);
+  // response.send("my custom 404 page");
+  // same thing, just shorthand
+  return res.status(404).send("my custom 404 page");
+});
 
 //server is now active
 app.listen(PORT, () => {
